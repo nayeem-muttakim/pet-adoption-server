18d692614bfdd487d79c6d4d941bad4826c23178
Nohan550 <nohanmuttakim.bd@gmail.com>
2023-11-29 15:47:09 +0600

    users collection added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors =require("cors")
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 const port = process.env.PORT || 5589;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bix9lir.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,29 +22,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-     const database = client.db('petsDB');
-     const categories = database.collection('categories');
-      const encourages = database.collection('encourage');
+    const database = client.db("petsDB");
+    const categories = database.collection("categories");
+    const encourages = database.collection("encourage");
+    const users = database.collection("users");
+
+    // users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert email if does not exist
+      const query = { email: user.email };
+  
+      const exist = await users.findOne(query);
+      if (exist) {
+        return res.send({ message: "user already exists", insertedId: null });
+      };
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
 
     //  Pets
 
-    app.get('/categories',async(req,res)=>{
-      const result =await categories.find().toArray();
-      res.send(result)
+    app.get("/categories", async (req, res) => {
+      const result = await categories.find().toArray();
+      res.send(result);
     });
 
-    app.get('/encourages',async(req,res)=>{
+    app.get("/encourages", async (req, res) => {
       const result = await encourages.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "successfully connected to MongoDB!"
-    );
+    console.log("successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
